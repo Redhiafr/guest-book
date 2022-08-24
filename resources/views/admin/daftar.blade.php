d045b8c812adb74ca8097c23f7f6c74da783edc1
nsenja <nabilasw34@gmail.com>
2022-08-24 15:46:58 +0700

    Cetak Daftar Tamu revisi

--- resources/views/admin/daftar.blade.php
+++ resources/views/admin/daftar.blade.php
@@ -15,7 +15,7 @@
 
         a {
             color: #5D6975;
-            text-decoration: underline;
+            text-decoration: underline;            
         }
 
         body {
@@ -25,7 +25,7 @@
             margin: 0 auto;
             color: #001028;
             background: #FFFFFF;
-            font-family: Arial, sans-serif;
+            font-family: Poppins, sans-serif;
             font-size: 14px;
             font-family: Arial;
         }
@@ -44,12 +44,17 @@
         }
 
         h2 {
-            border-bottom: 1px solid #5D6975;
-            color: #001028;
+                                   
+            font-weight: 400;
+            line-height: 1.5;
+            color: #858796;
+            font-family: Verdana, Geneva, Tahoma, sans-serif;            
+            margin-top: 10%;
             /* font-size: 2.4em; */
-            line-height: 1.4em;
-            font-weight: normal;
+            line-height: 1.4em;            
+            font-weight: bold;
             text-align: center;
+
             /* margin: 0 0 20px 0; */
             background: url(dimension.png);
         }
@@ -57,120 +62,56 @@
         h3 {
             /* border-bottom: 1px solid #5D6975; */
             color: #001028;
+            font-family: Verdana, Geneva, Tahoma, sans-serif;
             /* font-size: 2.4em; */
             /* line-height: 1.4em; */
             font-weight: normal;
             text-align: center;
-            /* margin: 0 0 20px 0; */
+            /*margin: 0 0 20px 0;*/
             background: url(dimension.png);
         }
 
-        #identity {
-            display: flex;
-        }
-
-        #project {
-            float: left;
-        }
-
-        #project span {
-            color: #5D6975;
-            text-align: right;
-            margin-right: 10px;
-            font-size: 0.9em;
-        }
-
-        #company {
-            text-align: center;
-        }
-
-        #project div,
-        #company div {
-            white-space: nowrap;
-        }
-
         table {
-            width: 100%;
+            width: 10%;
+            margin-top: 5%;
             border-collapse: collapse;
             border-spacing: 0;
-            margin-bottom: 20px;
         }
 
         table th,
-        table td {
+        table td {                        
             text-align: center;
+            font-family: Verdana, Geneva, Tahoma, sans-serif            
         }
 
         table th {
-            padding: 5px 20px;
+            padding: 10px 20px;            
             color: #5D6975;
-            border-bottom: 1px solid #5D6975;
+            border-bottom: 1px solid #858796;
             white-space: nowrap;
             font-weight: bold;
-        }
-
-        table .service,
-        table .desc {
-            text-align: left;
+            border-collapse: collapse;
         }
 
         table td {
-            padding: 15px;
-            text-align: center;
-        }
-
-        table td.service,
-        table td.desc {
-            vertical-align: top;
-        }
-
-        table td.qty {
-            text-align: center;
-        }
-
-        table td.service,
-        table td.unit,
-        table td.qty,
-        table td.total,
-        table td.grand {
-            font-size: 1.2em;
-        }
-
-        table td.grand {
-            border-top: 1px solid #5D6975;
-            font-weight: bold;
-        }
-
-        #notices .notice {
-            color: #5D6975;
-            font-size: 1.2em;
-        }
-
-        footer {
-            color: #5D6975;
-            width: 100%;
-            height: 30px;
-            position: absolute;
-            bottom: 0;
-            border-top: 1px solid #C1CED9;
-            padding: 8px 0;
+            padding: 10px;
             text-align: center;
         }
 
         .topright {
             position: absolute;
-            top: 8px;
+            top: 10px;
             right: 16px;
             font-size: 18px;
-            width: 15%;
+            width: 17%;
         }
 
         .topleft {
             position: absolute;
-            top: 8px;
+            top: 10px;
             left: 16px;
             font-size: 18px;
-            width: 15%;
+            width: 20%;
         }
     </style>
 </head>
@@ -180,58 +121,41 @@
         <img src="assets/images/logo-bumn1.png" class="topleft">
         <img src="assets/images/logo-4.png" class="topright">
     </header>
-    <h2>List Daftar Tamu</h2>
-    <h3>Pada Tanggal dd/mm/yyyy</h3>
+    <h2 style="text-decoration: underline;">DAFTAR TAMU</h2>
+    <h3>dd/mm/yyyy - dd/mm/yyyy</h3>
     <main>
-        <table class="table table-striped">
-            <thead>
-                <tr>
-                    <th>No</th>
-                    <th class="service">Nama</th>
-                    <th class="unit">No. Telepon</th>
-                    <th>Tujuan</th>
-                    <th>Kategori</th>
-                    <th>Nama Instansi</th>
-                    <th>Keterangan</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($guests as $u)
+        <div class="table-responsive" style="overflow-x:auto">
+            <table class="table">
+                <thead>
                     <tr>
-                        <td>{{ $loop->iteration }}</td>
-                        <td>{{ $u->nama }}</td>
-                        <td>{{ $u->telp }}</td>
-                        <td>{{ $u->tujuan }}</td>
-                        @foreach ($category as $cat)
-                            @if ($cat->id == $u->kategori_id)
-                                <td>{{ $cat->nama_kategori }}</td>
-                            @endif
-                        @endforeach
-                        <td>{{ $u->instansi }}</td>
-                        <td>{{ $u->keterangan }}</td>
-                        <td>
-                    <tr>
-                @endforeach
-
-
-                {{-- @foreach ($guests as $g)
-                    <tr>
-                        <td class="qty">{{ $loop->iteration }}</td>
-                        <td class="service">{{ $g->nama }}</td>  
-                        <td class="service">{{ $g->telp }}</td>  
-                        <td class="service">{{ $g->tujuan }}</td>  
-                        @foreach ($category as $cat)
-                        @if ($cat->id == $g->kategori_id)
-                            <td>{{ $cat->nama_kategori }}</td>
-                        @endif
-                        @endforeach
-                        <td class="service">{{ $g->instansi }}</td>  
-                        <td class="service">{{ $g->keterangan }}</td>  
-                       
+                        <th>No</th>
+                        <th>Nama</th>
+                        <th>No. Telepon</th>
+                        <th>Tujuan</th>
+                        <th>Kategori</th>
+                        <th>Nama Instansi</th>
+                        <th>Keterangan</th>
                     </tr>
-                @endforeach --}}
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    @foreach ($guests as $u)
+                        <tr>
+                            <td>{{ $loop->iteration }}</td>
+                            <td>{{ $u->nama }}</td>
+                            <td>{{ $u->telp }}</td>
+                            <td>{{ $u->tujuan }}</td>
+                            @foreach ($category as $cat)
+                                @if ($cat->id == $u->kategori_id)
+                                    <td>{{ $cat->nama_kategori }}</td>
+                                @endif
+                            @endforeach
+                            <td>{{ $u->instansi }}</td>
+                            <td>{{ $u->keterangan }}</td>                           
+                        <tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </main>
 
 </body>
